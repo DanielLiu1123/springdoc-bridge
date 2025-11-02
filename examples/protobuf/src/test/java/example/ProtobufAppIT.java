18442f6a4f98c1ab7d27955b93ffea69fb773ad4
Freeman Liu <llw599502537@gmail.com>
2025-11-02 10:57:16 +0800

    upgrade deps

--- examples/protobuf/src/test/java/example/ProtobufAppIT.java
+++ examples/protobuf/src/test/java/example/ProtobufAppIT.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.test.LocalServerPort;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.web.servlet.client.RestTestClient;
 import tools.jackson.databind.JsonNode;
